8d59f3b8a26122f85a0f7c4801070a44f21d1e34
Marcin Zielonka <zielonka.marcin@protonmail.com>
2025-12-01 12:04:38 +0100

    ✨ provided solution for day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,55 @@
+import kotlin.math.abs
+
 fun main() {
+
+    fun extract(line: String): Pair<Char, Int> = Pair(line.first(), line.drop(1).toInt())
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+
+        var current = 50
+        for (line in input) {
+            val (direction, amount) = extract(line)
+
+            when (direction) {
+                'L' -> current = (current - amount) % 100
+                'R' -> current = (current + amount) % 100
+            }
+
+            if (current == 0) result++
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var result = 0
+
+        /** Returns number of hundreds in given number **/
+        fun Int.hundreds(): Int = abs(this / 100)
+
+        var current = 50
+        for (line in input) {
+            val (direction, amount) = extract(line)
+
+            val new = when (direction) {
+                'L' -> current - amount
+                'R' -> current + amount
+                else -> error("Should not happen")
+            }
+
+            result += abs(new.hundreds() - current.hundreds())
+            if(new == 0) result++
+            if(current * new < 0) result++
+
+            current = new % 100
+        }
+        return result
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    with(part1(testInput)) { check(this == 3) { "Got instead: $this" } }
+    with(part2(testInput)) { check(this == 6) { "Got instead: $this" } }
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
